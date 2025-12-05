ddedfa667a049c6e52107736cf3168698e788af3
Gym-boy <chetan79871@gmail.com>
2025-12-06 00:55:02 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String...args){
+    RWOnly rwonly= new RWOnly();
+      rwonly.setName= "Chetan";
+      System.out.print(rwonly.getName());
+  }
 }
